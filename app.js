a5ea252780f69ea62413002f2b536fa385c248b2
Pedro Neto <pedro.diogo.neto@outlook.pt>
2018-02-26 17:54:37 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+
+const index = require('./routes/index');
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', index);
+
+// -- 404 and error handler
+
+// NOTE: requires a views/not-found.ejs template
+app.use((req, res, next) => {
+  res.status(404).json({ error: 'not-found' });
+});
+
+// NOTE: requires a views/error.ejs template
+app.use((err, req, res, next) => {
+  // always log the error
+  console.error('ERROR', req.method, req.path, err);
+
+  // only render if the error ocurred before sending the response
+  if (!res.headersSent) {
+    res.status(500).json({ error: 'there was an error' });
+  }
+});
+
+module.exports = app;
